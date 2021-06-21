43c1ff9346cad7702f75c4311b45d157a1d752e0
Raoni Normanton <raonifn@gmail.com>
2021-06-21 15:09:31 -0300

    vim changes

--- install/02-vim.sh
+++ install/02-vim.sh
@@ -23,7 +23,6 @@ done
 #install plugins
 #===================
 if [[ ! -e $vimConfigDir/autoload/plug.vim ]]; then
-  # git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
